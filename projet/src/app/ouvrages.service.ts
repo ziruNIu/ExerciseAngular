fdf377442b9223bc7dd0085d902a98f155ae3b92
Wilfantasiapp <86404028+Wilfantasiapp@users.noreply.github.com>
2021-06-29 10:17:13 +0200

    ajout de service et du routing ansi que du header

---
+++ projet/src/app/ouvrages.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { DATA } from 'src/data';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OuvragesService {
+  getOuvrages(): Observables <Variable>{
+  
+  }
+  constructor() { }
+}
