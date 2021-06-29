ae6b1f7cc1d10d32a61b0455dd35955a122c0768
Ziru Niu <ziru@fantasiapp.com>
2021-06-29 14:09:14 +0200

    routing for log in page

--- projet/src/app/app-routing.module.ts
+++ projet/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { ConnectionComponent } from './connection/connection.component';
+import { DetailComponent } from './detail/detail.component';
+const routes: Routes = [
+  {path: '', redirectTo: '/login',pathMatch:'full'},
+  {path: 'login', component: ConnectionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- projet/src/app/app.module.ts
+++ projet/src/app/app.module.ts
@@ -1,45 +1,33 @@
 import { RouterModule } from '@angular/router';
-import { OuvragesService } from './ouvrages.service';
 import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { ConnectionComponent } from './connection/connection.component';
 import { CheckboxComponent } from './checkbox/checkbox.component';
-import { HeaderComponent } from './header/header.component';
-import { OuvrageComponent } from './ouvrage/ouvrage.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { DetailComponent } from './detail/detail.component';
 
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './header/header.component';
 @NgModule({
   declarations: [
     AppComponent,
     ConnectionComponent,
     CheckboxComponent,
+    DetailComponent,
     HeaderComponent,
-    OuvrageComponent,
-    DetailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot([
-      { 
-        path:'', 
-        component: ConnectionComponent 
-      },
-      { 
-        path:'ouvrages', 
-        component: HeaderComponent 
-      },
-    ]),
     BrowserAnimationsModule,
     MatGridListModule
   ],
   providers: [
-    OuvragesService
+    // OuvragesService
   ],
   bootstrap: [AppComponent]
 })
